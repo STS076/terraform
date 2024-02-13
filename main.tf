11f51d68580c9d375674c4bb241cbebe03956a18
Sophie Toussaint <sophfrom76@hotmail.com>
2024-02-13 14:26:03 +0100

    kubernetes

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "appCube01" {
-  name     = "rg-${var.project_name}-${var.environment}-01"
+  name     = "rg-${var.project_name}-${var.environment}-cube01"
   location = "West Europe"
 
   tags = local.tags
